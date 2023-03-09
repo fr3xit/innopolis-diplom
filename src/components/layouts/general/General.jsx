9bc95ff91952697c8a30769285cb2118b43605b6
fr3xit <fr3xit@gmail.com>
2023-03-09 12:00:11 +0300

    09-03-2023:12:00

--- src/components/layouts/general/General.jsx
+++ src/components/layouts/general/General.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import mcnBind from 'classnames/bind';
 
 import Container from '@global/container/Container';
@@ -6,13 +7,35 @@ import styles from './General.module.scss';
 const SCN = mcnBind.bind(styles);
 
 const General = function ({ header, main, footer }) {
+	const elementHeader = useRef(null);
+	const elementMain = useRef(null);
+	const elementFooter = useRef(null);
+
+	useEffect(() => {
+		const extraIndent = 30;
+
+		elementMain.current.style.paddingTop = `${
+			elementHeader.current.offsetHeight + extraIndent
+		}px`;
+
+		elementMain.current.style.paddingBottom = `${
+			elementFooter.current.offsetHeight + extraIndent
+		}px`;
+	}, []);
+
 	return (
 		<div className={SCN('page')}>
-			<header className={SCN('page__header')}>
+			<header ref={elementHeader} className={SCN('page__header')}>
 				<Container child={header} />
 			</header>
-			<main className={SCN('page__main')}>{main()}</main>
-			<footer className={SCN('page__footer')}>footer</footer>
+
+			<main ref={elementMain} className={SCN('page__main')}>
+				{main()}
+			</main>
+
+			<footer ref={elementFooter} className={SCN('page__footer')}>
+				{footer ? footer() : ''}
+			</footer>
 		</div>
 	);
 };
