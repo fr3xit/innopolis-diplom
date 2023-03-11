e96a4267c5d94d4b5ed2ff6cec7843b8e1f7604b
fr3xit <fr3xit@gmail.com>
2023-03-11 13:47:38 +0300

    11-03-2023:13:47

--- src/components/page/products/Products.jsx
+++ src/components/page/products/Products.jsx
@@ -1,13 +1,13 @@
 import mcnBind from 'classnames/bind';
 import { Link } from 'react-router-dom';
+
 import Layout from '@layouts/general/General.jsx';
+import Container from '@global/container/Container';
 import Header from '@global/header/Header';
 import Title from '@global/header/elements/title/Title';
 import Basket from '@blocks/basket/Basket';
-import Container from '@global/container/Container';
 import ProductsList from '@blocks/productsList/ProductsList';
 
-import testFunc from '@js/testFunc';
 import styles from './Products.module.scss';
 
 const SCN = mcnBind.bind(styles);
