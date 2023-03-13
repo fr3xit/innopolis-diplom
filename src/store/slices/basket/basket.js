0b77a3776a94685ed154fb9178c5a7e559ffb3e3
fr3xit <fr3xit@gmail.com>
2023-03-13 08:20:17 +0300

    13-03-2023:08:20

--- src/store/slices/basket/basket.js
+++ src/store/slices/basket/basket.js
@@ -1,18 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { useSelector } from 'react-redux';
 
+const productList = require('@data/productsData.json');
+
 const initState = {
 	amount: 0,
-	total: 0,
-	list: [],
+	sum: 0,
+	list: [...productList],
 };
+
+function GetState(state) {
+	const data = useSelector(state => state.product);
+	console.log(data);
+	// console.log(state.product);
+	// return state.product;
+}
 
 export const basketSlice = createSlice({
 	name: 'basket',
 	initialState: initState,
 	reducers: {
 		addProduct: (state, action) => {
-			state.list.push(action.payload);
+			state.sum = state.list.reduce((sum, item) => {
+				state.amount++;
+				return sum + Number(item.price.replace(/\s/g, ''));
+			}, 0);
+
+			// state.list.push(action.payload);
 		},
 	},
 });
