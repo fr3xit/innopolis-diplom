d06006c6f09338d3ce571d86bde2cf998baf0470
fr3xit <fr3xit@gmail.com>
2023-03-19 10:39:34 +0300

    19-03-2023:10:39

--- src/components/blocks/product/Product.jsx
+++ src/components/blocks/product/Product.jsx
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import moduleClassNameBind from 'classnames/bind';
-import { useParams } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 
 import ToPay from '@modules/toPay/ToPay';
 import Button from '@modules/button/Button';
@@ -8,36 +8,39 @@ import Button from '@modules/button/Button';
 import stylesProduct from './Product.module.scss';
 const classNameProduct = moduleClassNameBind.bind(stylesProduct);
 const Product = function () {
+	const allProduct = useSelector(state => state.product.allProduct);
 	const { id } = useParams();
-	console.log(id);
-	const {
-		title,
-		price,
-		description: { long: desc },
-		img: { url },
-	} = useSelector(state =>
-		state.product.allProduct.find(item => item.productId === Number(id))
-	);
 
-	return (
-		<div className={classNameProduct('product')}>
-			<div className={classNameProduct('product__img')}>
-				<img src={url} alt="" />
-			</div>
+	if (id > allProduct.length) {
+		return <Navigate to="/notFound" />;
+	} else {
+		const {
+			title,
+			price,
+			description: { long: desc },
+			img: { url },
+		} = allProduct.find(item => item.productId === Number(id));
 
-			<div className="product__content">
-				<h3 className={classNameProduct('product__title')}>{title}</h3>
-				<div className={classNameProduct('product__desc')}>
-					<p>{desc}</p>
+		return (
+			<div className={classNameProduct('product')}>
+				<div className={classNameProduct('product__img')}>
+					<img src={url} alt="" />
 				</div>
 
-				<div className={classNameProduct('product__footer')}>
-					<ToPay sum={price} />
-					<Button>В корзину</Button>
+				<div className="product__content">
+					<h3 className={classNameProduct('product__title')}>{title}</h3>
+					<div className={classNameProduct('product__desc')}>
+						<p>{desc}</p>
+					</div>
+
+					<div className={classNameProduct('product__footer')}>
+						<ToPay sum={price} />
+						<Button>В корзину</Button>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 };
 
 export default Product;
