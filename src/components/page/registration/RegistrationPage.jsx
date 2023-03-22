f8f9432fb78f3e1103822099dc622c97ce08b517
fr3xit <fr3xit@gmail.com>
2023-03-22 21:15:38 +0300

    22-03-2023:21:15

--- src/components/page/registration/RegistrationPage.jsx
+++ src/components/page/registration/RegistrationPage.jsx
@@ -1,18 +1,12 @@
 import moduleClassNameBind from 'classnames/bind';
 
+import { registration } from '@tools/formTools';
 import Layout from '@layouts/simple/Simple.jsx';
 import Container from '@global/container/Container';
 import FormRegAut from '@blocks/formRegAut/FormRegAut';
 
 import stylesLayout from '@layouts/simple/Simple.module.scss';
 const classNameLayout = moduleClassNameBind.bind(stylesLayout);
-
-const registration = ({ email, password, agree }) => {
-	console.log('registration');
-	console.log('email = ', email);
-	console.log('password = ', password);
-	console.log('agree = ', agree);
-};
 
 const RegistrationPage = function () {
 	return (
--- src/tools/formTools.js
+++ src/tools/formTools.js
@@ -1,3 +1,5 @@
+import { getLocalStorage } from '@tools/restTools';
+
 export const emailValidation = email => {
 	const re =
 		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -54,4 +56,15 @@ export const passwordValidation = pass => {
 	}
 
 	return result;
+};
+
+export const registration = user => {
+	let usersDataBase = getLocalStorage('users');
+
+	if (!usersDataBase) {
+		usersDataBase = [];
+	}
+
+	console.log(usersDataBase);
+	console.log(user);
 };
