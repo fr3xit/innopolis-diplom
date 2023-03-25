15aec1256ec945871d819b880ae03de6f0bef387
fr3xit <fr3xit@gmail.com>
2023-03-25 21:45:01 +0300

    25-03-2023:21:45

--- src/components/page/test/TestPage.jsx
+++ src/components/page/test/TestPage.jsx
@@ -6,10 +6,10 @@ import './TestPage.scss';
 
 // const container = document.getElementById('root');
 // const root = ReactDOM.createRoot(container);
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 export const TestPage = function () {
 	const handler = () => {
-		root.render(<Modal />);
+		// root.render(<Modal />);
 		// ReactDOM.render(<Modal />, document.getElementById('root'));
 	};
 	return (
